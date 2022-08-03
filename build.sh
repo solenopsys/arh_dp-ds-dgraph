e47531bb531d5f33a8b9b6ddc520daa5cbe31bf8
alexstorm <solenopsys:)>
2022-08-03 12:26:29 +0300

    build changes

--- build.sh
+++ build.sh
@@ -8,17 +8,18 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   # rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.local/api/charts
+  curl --data-binary "@${FN}" http://helm.alexstorm.solenopsys.org/api/charts
 }
 
 REGISTRY=registry.alexstorm.solenopsys.org
 NAME=solenopsys-ds-dgraph
 ARCHS="linux/amd64,linux/arm64"
-VER=0.1.0
+VER=0.1.3
+
 
 
 helm_build_push
-build_push
+#build_push
 
 
 
